7af03b1e4778ded97ba9ba2f66f7f27f7cdb3d98
onojastanley <40136094+onojastanley@users.noreply.github.com>
2020-09-17 14:36:23 +0100

    Add files via upload

---
+++ src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Landing from './landing';
+import AboutMe from './about';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route path="/about" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+)
+
+export default Main;
